b99f697dd5e22f866603597d735b0de702ed1f68
jmuidesign <julienmerlin.dev@gmail.com>
2025-03-21 08:46:55 +0100

    feature: remove liquidity

--- protocole/src/interfaces/IPiscineV1Exchange.sol
+++ protocole/src/interfaces/IPiscineV1Exchange.sol
@@ -7,10 +7,13 @@ interface IPiscineV1Exchange {
     error AddressZero();
     error AmountZero();
     error PoolAlreadyExists();
+    error PoolDoesNotExist();
 
     event PoolCreated(address pool, address token0, address token1);
     event LiquidityAdded(address token0, address token1, uint256 amount0, uint256 amount1);
+    event LiquidityRemoved(address token0, address token1, uint256 lpTokensAmount);
 
     function createPool(address tokenA, address tokenB) external returns (address pool);
     function addLiquidity(address tokenA, address tokenB, uint256 amountA, uint256 amountB) external;
+    function removeLiquidity(address tokenA, address tokenB, uint256 lpTokensAmount) external;
 }
